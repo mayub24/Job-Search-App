553ce6f22b811480ca5163b356adb69c01e96894
MuhammadAyub24 <43295653+MuhammadAyub24@users.noreply.github.com>
2025-12-07 11:43:06 -0500

    passing jobsChanged into jobPage component

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Jobs from '../components/Jobs'
 
-const JobsPage = ({job_url}) => {
+const JobsPage = ({job_url, jobsChanged}) => {
   return (
     <>
       <section className='bg-blue-50 px-4 py-6'>
-            <Jobs isJobsPage = {true} numberOfJobs={10} job_url={job_url} />
+            <Jobs isJobsPage = {true} numberOfJobs={10} job_url={job_url} jobsChanged={jobsChanged} />
       </section>
     </>
   )
